8825eeb008ea66dbc0535fd46037ee16b0e94b86
Irutehe <b76bra@yahoo.com>
2015-06-13 18:43:01 +0300

    added list baskte

--- h2015/src/AppBundle/Controller/DefaultController.php
+++ h2015/src/AppBundle/Controller/DefaultController.php
@@ -2,8 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\HBasket;
 use AppBundle\Entity\HProducts;
 use AppBundle\Entity\HWishlist;
+use AppBundle\Repository\HBasketRepository;
+use AppBundle\Repository\HProductsRepository;
 use AppBundle\Repository\HWishlistRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -23,7 +26,7 @@ class DefaultController extends Controller
     public function homepageAction(Request $request)
     {
         $entityManager = $this->getDoctrine()->getManager();
-        /** @var \HProductsRepository $productsRepo */
+        /** @var HProductsRepository $productsRepo */
         $productsRepo = $entityManager->getRepository(HProducts::REPOSITORY);
         $filters      = $request->query;
         $products     = $productsRepo->getAllProductsByFilters($filters);
@@ -66,5 +69,47 @@ class DefaultController extends Controller
         /** @var HWishlistRepository $wishlistRepo */
         $wishlistRepo = $entityManager->getRepository(HWishlist::REPOSITORY);
 
+    }
+
+    public function listBasketAction(Request $request)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        /** @var HBasketRepository $basketRepo */
+        $basketRepo     = $entityManager->getRepository(HBasket::REPOSITORY);
+        $basketProducts = $basketRepo->findAll();
+        $results        = array();
+
+        if (count($basketProducts) > 0) {
+            /** @var HBasket $basketProduct */
+            foreach ($basketProducts as $basketProduct) {
+                $product                   = $basketProduct->getHProducts();
+                $results['basket_items'][] = array(
+                    "id"       => $basketProduct->getId(),
+                    "product"  => array(
+                        "id"                    => $product->getId(),
+                        "name"                  => $product->getName(),
+                        "brand"                 => $product->getHBrands()->getName(),
+                        "category"              => $product->getHCategories()->getName(),
+                        "price"                 => $product->getPrice(),
+                        "discount"              => $product->getDiscount(),
+                        "deliveryEstimatedCost" => $product->getDeliveryEstimatedCost(),
+                        "status"                => $product->getStatus()
+                    ),
+                    "quantity" => $basketProduct->getQuantity()
+                );
+            }
+
+        }
+
+        $isJsonP = $request->get('callback');
+
+
+        if ($isJsonP) {
+
+            echo $isJsonP . '(' . json_encode($results) . ');';
+            die;
+        }
+
+        return new JsonResponse($results);
     }
 }
